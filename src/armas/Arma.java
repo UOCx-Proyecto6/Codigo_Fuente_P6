7f3b7634c642876131b8de51f3bfe2e046912f8b
xabertum <mohatar@gmail.com>
2018-10-26 13:32:45 +0200

    Armas Work

--- src/armas/Arma.java
+++ src/armas/Arma.java
@@ -4,7 +4,16 @@ import java.util.HashMap;
 
 public interface Arma {
 	
-	public HashMap<String, String> arma = new HashMap<String, String> ();
+	public HashMap<String, Integer> armasAtaque = new HashMap<String, Integer> ();
+	public HashMap<String, Integer> armasDefensa = new HashMap<String, Integer> ();
+	
+	
+	public static void armasAtaque() {
+		armasAtaque.put("Espada", 1);
+		armasAtaque.put("Hacha", 2);
+		armasAtaque.put("Martillo", 3);
+		armasAtaque.put("Arco", 4);
+	}
 	
 	
 
